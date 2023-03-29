153ffd8131302d5dd28989bc57fabc6a1fce461d
Gastón Barrionuevo <gaston.cba97@gmail.com>
2023-03-29 11:26:36 -0300

    change confg.

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,14 @@ import cors from 'cors'
 import { routerApi } from './routes';
 import { logError, errorHandler, boomErrorHandler} from './middleware/error.handler';
 import { appDataSource } from "./database/database";
+import { config } from "./config/config";
 
 appDataSource.initialize()
   .then(() => console.log("Conexion exitosa"))
   .catch((err) => console.log("Error de conexion ", err))
 
 const app = express();
-const port = 3000;
+const port = config.port;
 
 app.use(express.json())
 app.use(cors())
